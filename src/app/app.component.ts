cd025e67f04da69570def67ec489341807c9c155
Kiwi-7 <60024260+Kiwi-7@users.noreply.github.com>
2024-06-13 21:51:32 +0700

    mommit 2nd

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,13 +6,17 @@ import { NewReleaseComponent } from './new-release/new-release.component';
 import { RouterLink } from '@angular/router';
 import { RouterLinkActive } from '@angular/router';
 import { CommonModule } from '@angular/common';
+import { BookInfoComponent } from './book-info/book-info.component';
+import { CartComponent } from './cart/cart.component';
+import { SearchResultsComponent } from './search-results/search-results.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
     RouterOutlet,BooksComponent,AdvanceSearchComponent,NewReleaseComponent,
-    RouterLink,RouterLinkActive,CommonModule
+    RouterLink,RouterLinkActive,CommonModule,BookInfoComponent,CartComponent,
+    SearchResultsComponent
   ], 
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,15 @@ import { Routes } from '@angular/router';
 import { BooksComponent } from './books/books.component';
 import { AdvanceSearchComponent } from './advance-search/advance-search.component';
 import { NewReleaseComponent } from './new-release/new-release.component';
+import { BookInfoComponent } from './book-info/book-info.component';
+import { CartComponent } from './cart/cart.component';
+import { SearchResultsComponent } from './search-results/search-results.component';
 
 export const routes: Routes = [
+   { path: '', component: SearchResultsComponent},
    { path: 'books', component: BooksComponent},
    { path: 'advance-search', component: AdvanceSearchComponent},
-   { path: 'new-release', component: NewReleaseComponent}
+   { path: 'new-release', component: NewReleaseComponent},
+   { path: 'book-info', component: BookInfoComponent},
+   { path: 'cart', component: CartComponent}
 ];
